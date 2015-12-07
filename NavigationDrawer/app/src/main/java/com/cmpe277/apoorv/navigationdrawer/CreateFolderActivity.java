a83049a7a051a73ca16ba2aa257826d2c5489a64
apoorv <apoorv.agrawal02@gmail.com>
2015-12-06 18:22:26 -0800

    Connection to gmail

---
+++ NavigationDrawer/app/src/main/java/com/cmpe277/apoorv/navigationdrawer/CreateFolderActivity.java
@@ -0,0 +1,13 @@
+package com.cmpe277.apoorv.navigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateFolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_folder);
+    }
+}
--- NavigationDrawer/app/src/main/java/com/cmpe277/apoorv/navigationdrawer/NavDrawer.java
+++ NavigationDrawer/app/src/main/java/com/cmpe277/apoorv/navigationdrawer/NavDrawer.java
@@ -1,10 +1,10 @@
 package com.cmpe277.apoorv.navigationdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,11 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NavDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     NavigationView navigationView=null;
     Toolbar toolbar=null;
+    Toolbar toolbar1=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +67,7 @@ public class NavDrawer extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.nav_drawer, menu);
         return true;
+
     }
 
     @Override
@@ -77,6 +80,11 @@ public class NavDrawer extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if(id== R.id.navigate)
+        {
+            startActivity(new Intent(this,SubActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
